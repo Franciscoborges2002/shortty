e6e445518210f27d39c8164f14119271df7981a5
Francisco <oficialkikolms123@gmail.com>
2020-08-17 23:45:06 +0100

    An introduction of the slug system.

--- src/controllers/shorttyController.js
+++ src/controllers/shorttyController.js
@@ -4,34 +4,104 @@ const validURL = require('valid-url');
 
 async function createURL(req, res) {
     const url = req.body.url;
-    const slug = nanoid(6);
+    const bodySlug = req.body.slug;
+    //const nanoid = nanoid(6);
     const baseURL = 'http://localhost:5000'
 
 
     if(validURL.isUri(url)){
         try{
             const _url = await URLSchema.findOne({ url });//Veify if there is the same url in the database
-            //const _slug = await URLSchema.findOne({ slug });//Veify if there is the same slug in the database
+
 
             if(_url){//If is one _url send it
                 res.send(_url);
             }else{
-                let _url = baseURL + '/' + slug;
+                if(bodySlug == ''){
+                    const slug = nanoid(6);
+                    let _url = baseURL + '/' + slug;
 
-                const shortty = new URLSchema({
-                    url: url,
-                    slug: slug,
-                    date: new Date()
-                });
+                    const shortty = new URLSchema({
+                        url: url,
+                        slug: slug,
+                        date: new Date() 
+                    });
 
-                shortty.save().then(()=>{
-                    console.log('URL received');
-                }).catch((err)=>{
-                    console.log('URL not received: ' + err)
-                    res.status(500).send({message: err});
-                });
-            
-                res.send(_url);
+                    shortty.save().then(()=>{
+                        console.log('URL received');
+                    }).catch((err)=>{
+                        console.log('URL not received: ' + err)
+                        res.status(500).send({message: err});
+                    });
+
+                     return res.send(_url);
+                }
+        
+                if(bodySlug){
+                    var _slug = await URLSchema.find({ slug: bodySlug });//Verify if there is the same slug in the database
+                    var slug = '';
+
+                    if(_slug){
+                        do{
+                            slug = nanoid(6);
+    
+                            _slug = await URLSchema.find({ slug });
+                        } while(_slug)  
+
+                        
+                        let _url = baseURL + '/' + slug;
+
+                        const shortty = new URLSchema({
+                            url: url,
+                            slug: slug,
+                            date: new Date() 
+                        });
+
+                        shortty.save().then(()=>{
+                            console.log('URL received');
+                        }).catch((err)=>{
+                            console.log('URL not received: ' + err)
+                            res.status(500).send({message: err});
+                        });
+
+                        res.send(_url);
+                    } else {
+                        
+                    let _url = baseURL + '/' + bodySlug;
+
+                    const shortty = new URLSchema({
+                        url: url,
+                        slug: bodySlug,
+                        date: new Date() 
+                    });
+
+                    shortty.save().then(()=>{
+                        console.log('URL received');
+                    }).catch((err)=>{
+                        console.log('URL not received: ' + err)
+                        res.status(500).send({message: err});
+                    });
+
+                    res.send(_url);
+                    }
+
+                } else {
+
+                    const shortty = new URLSchema({
+                        url: url,
+                        slug: slug,
+                        date: new Date()
+                    });
+
+                    shortty.save().then(()=>{
+                        console.log('URL received');
+                    }).catch((err)=>{
+                        console.log('URL not received: ' + err)
+                        res.status(500).send({message: err});
+                    });
+                
+                    res.send(_url);
+                }
             }
         }catch(err){
             console.log(err);
